6f17a8d482992b7ad14c40c50b63532b3e9d0632
DavidFM2004 <1410616@alu.murciaeduca.es>
2025-11-11 13:46:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dam-ads-u2-equipoMolina
+Practica UT2 persistencia a base de datos
---
+++ src/servicio/ClubDeportivo.java
@@ -0,0 +1,137 @@
+package servicio;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import modelo.*;
+
+public class ClubDeportivo {
+    Connection con;
+    private final String url = "jdbc:mysql://localhost:3306/club_dama";
+    private final String user = "root";
+    private final String password = "1234";
+
+    public ClubDeportivo() throws SQLException {
+         con = DriverManager.getConnection(url, user, password);
+    }
+
+    public ArrayList<Socio> getSocios() {
+        ArrayList<Socio> socios = new ArrayList<>();
+        String stmt = "SELECT * FROM socios";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String dni;
+        String idSocio;
+        String nombre;
+        String apellidos;
+        String telefono;
+        String email;
+
+        try {
+            pstmt = con.prepareStatement(stmt);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                dni = (rs.getString("dni"));
+                idSocio = (rs.getString("id_socio"));
+                nombre = (rs.getString("nombre"));
+                apellidos = (rs.getString("apellidos"));
+                telefono = (rs.getString("telefono"));
+                email = (rs.getString("email"));
+                Socio socio = new Socio(idSocio,dni,nombre,apellidos,telefono,email);
+                socios.add(socio);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return socios;
+    }
+
+    public ArrayList<Pista> getPistas() {
+        ArrayList<Pista> pistas = new ArrayList<>();
+        String stmt = "SELECT * FROM pistas";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String idPista;
+        String deporte;
+        String descripcion;
+        Boolean disponible;
+
+
+        try {
+            pstmt = con.prepareStatement(stmt);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                idPista = (rs.getString("id_pista"));
+                deporte = (rs.getString("deporte"));
+                descripcion = (rs.getString("descripcion"));
+                disponible = (rs.getInt("disponible") == 1);
+                Pista pista = new Pista(idPista,deporte,descripcion,disponible);
+                pistas.add(pista);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return pistas;
+    }
+
+    public ArrayList<Reserva> getReservas() {
+        ArrayList<Reserva> reservas = new ArrayList<>();
+        String stmt = "SELECT * FROM reservas";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String idReserva;
+        String idSocio;
+        String idPista;
+        LocalDate fecha;
+        LocalTime hora_inicio;
+        Integer duracion_min;
+        Double precio;
+
+        try {
+            pstmt = con.prepareStatement(stmt);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                idReserva = (rs.getString("id_reserva"));
+                idSocio = (rs.getString("id_socio"));
+                idPista = (rs.getString("id_pista"));
+                fecha = rs.getDate("fecha").toLocalDate();
+                hora_inicio = rs.getTime("hora_inicio").toLocalTime();
+                duracion_min = rs.getInt("duracion_min");
+                precio = rs.getDouble("precio");
+                Reserva reserva = new Reserva(idReserva,idSocio,idPista,fecha,hora_inicio,duracion_min,precio);
+                reservas.add(reserva);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return reservas;
+    }
+}
